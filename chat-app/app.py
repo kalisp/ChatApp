825d6a6f9c19a045f823fde0fad3537533e53cf5
petr.kalis <petr.kalis@gmail.com>
2020-03-19 18:56:09 +0100

    Basic API implemented

--- chat-app/app.py
+++ chat-app/app.py
@@ -1,30 +1,111 @@
-from chalice import Chalice
+from chalice import Chalice, BadRequestError
 
-from model import user
+from model import user, room, post
 import uuid
 from datetime import datetime
+import hashlib
+import json
 
 app = Chalice(app_name='chat-app')
-
 
 @app.route('/')
 def index():
     return {'hello': 'World'}
 
+@app.route('/rooms')
+def rooms():
+    ''' List all room names '''
+    names = []
+    for r in room.Room.scan():
+        names.append(r.name)
+        print(r.name)
+
+    return json.dumps(names)
+
+@app.route('/room/create', methods=['GET', 'POST'])
+def create_room():
+    ''' Create new room '''
+    new_room = room.Room(
+        id = str(uuid.uuid1()),
+        name = "Room1",
+        read_only = False,
+        created_dt = datetime.utcnow(),
+        last_updated_dt = datetime.utcnow()
+    )
+    new_room.save()
+
+    return {'200': "Room created"}
+
+@app.route('/room/archive/{id}', methods=['GET', 'POST'])
+def archive_room(id):
+    ''' Deactivate room, user won't be able to post messages '''
+    try:
+        r = room.Room(id=id)
+        r.set_read_only(True)
+    except KeyError:
+        raise BadRequestError('Room with id: {} not found'.format(id))
+
+    return {'200' : "Room archived"}
+
+@app.route('/room/num_of_users/{id}', methods=['GET'])
+def num_of_users(id):
+    try:
+        r = room.Room(id=id)
+        numb = r.number_of_users()
+        return {'200': numb}
+    except KeyError:
+        raise BadRequestError('Room with id: {} not found'.format(id))
+
+@app.route('/room/add_user/room_id/{room_id}/user_id/{user_id}', methods=['GET', 'POST'])
+def add_user(room_id, user_id):
+    ''' Deactivate room, user won't be able to post messages '''
+    try:
+        r = room.Room(id=room_id)
+        try:
+            u = user.User(id=user_id)
+        except KeyError:
+            raise BadRequestError('User with id: {} not found'.format(user_id))
+        r.add_user(u.id)
+        u.join_room(r.id)
+    except KeyError:
+        raise BadRequestError('Room with id: {} not found'.format(id))
+
+    return {'200' : "Room archived"}
+
+@app.route('/room/kick_user/room_id/{room_id}/user_id/{user_id}', methods=['GET', 'POST'])
+def kick_user(room_id, user_id):
+    ''' Deactivate room, user won't be able to post messages '''
+    try:
+        r = room.Room(id=room_id)
+        try:
+            u = user.User(id=user_id)
+        except KeyError:
+            raise BadRequestError('User with id: {} not found'.format(user_id))
+        r.kick_user(u.id)
+        u.leave_room(r.id)
+    except KeyError:
+        raise BadRequestError('Room with id: {} not found'.format(id))
+
+    return {'200' : "Room archived"}
+
 @app.route('/users')
 def users():
+    ''' List all users '''
+    names = []
     for u in user.User.scan():
-        print(u)
-    return user.User.dumps()
+        names.append(u.name)
+
+    return json.dumps(names)
 
 @app.route('/user/create', methods=['GET', 'POST'])
 def create_user():
+    ''' Create new user '''
     new_user = user.User(
         id = str(uuid.uuid1()),
         first_name = "John",
         last_name = "Doe",
         email = "john@doe.com",
-        password = "John",
+        password = hashlib.sha3_512("John"),
         active = 1,
         rooms = [],
         created_dt = datetime.utcnow(),
@@ -32,6 +113,116 @@ def create_user():
     )
     new_user.save()
 
+    return {'200': "User created"}
+
+@app.route('/user/join_room/room_id/{room_id}/user_id/{user_id}', methods=['GET', 'POST'])
+def join_room(room_id, user_id): # not sure if needed/wanted
+    add_user(room_id, user_id)
+
+@app.route('/user/disable/{id}', methods=['GET', 'POST'])
+def disable_user(id):
+    ''' Deactivate user, user won't be able to post messages '''
+    try:
+        us = user.User(id=id)
+        us.set_active(False)
+    except KeyError:
+        raise BadRequestError('User with id: {} not found'.format(id))
+
+    return {'200' : "User deactivated"}
+
+@app.route('/user/activate/{id}', methods=['GET', 'POST'])
+def activate_user(id):
+    ''' Activate user, user will be able to post messages '''
+    try:
+        us = user.User(id=id)
+        us.set_active(True)
+    except KeyError:
+        raise BadRequestError('User with id: {} not found'.format(id))
+
+    return {'200' : "User activated"}
+
+@app.route('/user/change_password/{id}/{pass}', methods=['GET', 'POST'])
+def change_password(id, password):
+    try:
+        us = user.User(id=id)
+        us.set_password(password) # TODO switch to Cognito!
+    except KeyError:
+        raise BadRequestError('User with id: {} not found'.format(id))
+
+    return {'200' : "Password changed"}
+
+@app.route('/user/change_nick/{id}/{nick}', methods=['GET', 'POST'])
+def change_nick(id, nick):
+    try:
+        us = user.User(id=id)
+        us.set_nick(nick)  # TODO switch to Cognito!
+    except KeyError:
+        raise BadRequestError('User with id: {} not found'.format(id))
+
+    return {'200' : "Nick changed"}
+
+@app.route('/user/num_of_rooms/{id}', methods=['GET'])
+def num_of_rooms(id):
+    try:
+        us = user.User(id=id)
+        numb = us.get_number_joined_rooms()
+        return {'200': numb}
+    except KeyError:
+        raise BadRequestError('User with id: {} not found'.format(id))
+
+# ----------POSTS--------------------
+@app.route('/posts/room_id/{room_id}')
+def posts(room_id):
+    ''' Return all posts for specific room '''
+    # TODO add limit, order by created desc
+    try:
+        r = room.Room(id=id)
+        posts  = post.Post(room_id=r.id).scan()
+        return json.dumps(posts)
+    except KeyError:
+        raise BadRequestError('Room with id: {} not found'.format(id))
+
+    return json.dumps(names)
+
+@app.route('/post', methods=['POST'])
+def post():
+    body = app.current_request.json_body
+    try:
+        r = room.Room(id=body['room_id'])
+        try:
+            u = user.User(id=body['user_id'])
+        except KeyError:
+            raise BadRequestError('User with id: {} not found'.format(body['user_id']))
+        r.kick_user(u.id)
+        u.leave_room(r.id)
+        new_post = post.Post(id = str(uuid.uuid1()),
+                         room_id = r.id,
+                         user_id = u.id,
+                         content = body['content'],
+                         created_dt = datetime.utcnow(),
+                         last_updated_dt = datetime.utcnow()
+                         )
+        new_post.save()
+
+        return {'200': new_post.id}
+    except KeyError:
+        raise BadRequestError('Room with id: {} not found'.format(body['room_id']))
+
+@app.route('/post/id/{post_id}/{type}/user_id/{user_id}')
+def posts(post_id, type):
+    ''' Like/dislike post_id '''
+    try:
+        p = post.Post(id=post_id)
+        if type == 'like':
+            p.like(True)
+        elif type == 'dislike':
+            p.like(False)
+        else:
+            raise BadRequestError('Wrong type {} of affection'.format(type))
+        return {'200' : 'Post affected'}
+
+    except KeyError:
+        raise BadRequestError('Room with id: {} not found'.format(id))
 
 # @app.route('/users', methods=['POST'])
 # def create_user():
@@ -45,4 +236,7 @@ def create_user():
 
 if __name__ == '__main__':
     #create_user()
-    users()
+    #users()
+   #print(disable_user('1bf48208-69f5-11ea-a502-28e347aeb22f'))
+    #create_room()
+    print(rooms())
--- chat-app/model/user.py
+++ chat-app/model/user.py
@@ -1,7 +1,9 @@
 from pynamodb.models import Model
-from pynamodb.attributes import UnicodeAttribute, UnicodeSetAttribute, UTCDateTimeAttribute, BinaryAttribute
+from pynamodb.attributes import UnicodeAttribute, UnicodeSetAttribute, UTCDateTimeAttribute, BooleanAttribute
 import uuid
 import json
+from datetime import datetime
+import hashlib
 
 from config import Config
 
@@ -20,9 +22,49 @@ class User(Model):
     email = UnicodeAttribute()
     password = UnicodeAttribute()
     rooms = UnicodeSetAttribute(null=True)
-    active = BinaryAttribute(default=True)
+    active = BooleanAttribute(default=True)
     created_dt = UTCDateTimeAttribute()
     last_updated_dt = UTCDateTimeAttribute()
+
+    def set_active(self, state):
+        self.active = not state
+        self.update(actions=[
+            User.active.set(self.active),
+            User.last_updated_dt.set(datetime.now()),
+        ])
+
+    def set_password(self, password):
+        self.password = hashlib.sha3_512(password)
+        self.update(actions=[
+            User.password.set(self.password),
+            User.last_updated_dt.set(datetime.now()),
+        ])
+
+    def set_nick(self, nick):
+        self.nick = nick
+        self.update(actions=[
+            User.nick.set(self.nick),
+            User.last_updated_dt.set(datetime.now()),
+        ])
+
+    def join_room(self, room_id):
+        ''' Add room_id to 'rooms', opposite to Room.add_user(user_id) '''
+        self.rooms.append(room_id)
+        self.update(actions=[
+            User.rooms.set(self.rooms),
+            User.last_updated_dt.set(datetime.now()),
+        ])
+
+    def leave_room(self, room_id):
+        ''' Remove room_id from 'rooms', opposite to Room.kick_user(user_id) '''
+        self.rooms.remove(room_id)
+        self.update(actions=[
+            User.rooms.set(self.rooms),
+            User.last_updated_dt.set(datetime.now()),
+        ])
+
+    def get_number_joined_rooms(self):
+        return len(self.rooms)
 
     def __repr__(self):
         return 'User: id:{}, nick:{}, password:{}, created_dt:{}'.format(self.id, self.nick, self.password, self.created_dt)
